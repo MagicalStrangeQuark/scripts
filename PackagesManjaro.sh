617f5dbf6c919035ba8f04d4eb74805144b8a22a
Wesley Flôres <wesleyfloresterres@gmail.com>
2020-01-12 12:40:49 -0300

    Adicionado Pacotes Manjaro

--- PackagesManjaro.sh
+++ PackagesManjaro.sh
@@ -1,3 +1,17 @@
+## DEVILSPIE
+sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
+sudo pacman -S devilspie || sudo pacman -Rs devilspie
+mkdir -p ~/.devilspie
+
+echo '
+(if (contains (window_class) "Code")
+	(begin
+		(spawn_async (str "xprop -id " (window_xid) " -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 "))
+		(spawn_async (str "xprop -id " (window_xid) " -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY 0xD8000000"))
+	)
+)
+' > ~/.devilspie/vscode_transparent.ds
+
 ## GEOIP CONFIGURATION
 sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
 sudo pacman-mirrors -g --geoip
@@ -18,6 +32,17 @@ sudo yay -S visual-studio-code-bin
 sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
 sudo pacman -Sy git
 
+## NODE
+sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
+snap install --edge node
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+snap install --edge node --classic
+
+## P7ZIP
+sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
+sudo pacman -Sy p7zip
+
 ## PHP
 sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
 sudo pacman -Sy php
@@ -37,27 +62,10 @@ systemctl enable postgresql.service
 systemctl start postgresql.service
 systemctl status postgresql.service
 
+## UNRAR
+sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
+sudo pacman -Sy unrar
+
 ## YOUTUBE-DL
 sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
 sudo pacman -Sy youtube-dl
-
-## NODE
-sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
-snap install --edge node
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
-snap install --edge node --classic
-
-## DEVILSPIE
-sudo pacman -Sy && sudo pacman -Su && sudo pacman -Syu
-sudo pacman -S devilspie || sudo pacman -Rs devilspie
-mkdir -p ~/.devilspie
-
-echo '
-(if (contains (window_class) "Code")
-	(begin
-		(spawn_async (str "xprop -id " (window_xid) " -f _KDE_NET_WM_BLUR_BEHIND_REGION 32c -set _KDE_NET_WM_BLUR_BEHIND_REGION 0 "))
-		(spawn_async (str "xprop -id " (window_xid) " -f _NET_WM_WINDOW_OPACITY 32c -set _NET_WM_WINDOW_OPACITY 0xD8000000"))
-	)
-)
-' > ~/.devilspie/vscode_transparent.ds
